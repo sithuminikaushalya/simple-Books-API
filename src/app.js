824652808255944e6f7d302400c1314e96bb9d22
sithumini Kaushalya <kaushalyasithumini29@gmail.com>
2025-01-29 11:50:23 +0530

    solve issue

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const bookRoutes = require('./routes/bookRoutes');
-const swaggerDocument = require('./swagger.json'); 
 
 const app = express();
 
@@ -10,8 +9,6 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use('/books', bookRoutes);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => res.send('Books API is running'));
 
